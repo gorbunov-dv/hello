74a6826ddcf1d3ebf0cfb0c5e3255e395e8d612a
Dmitry <gorbunov.webdev@gmail.com>
2018-02-12 00:55:01 +0400

    show new variable in app

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 
 class Dropdown extends Component {
-  constructor(props) {
+    constructor(props) {
       super(props);
       this.state = { isOpened: false };
-  }  
+    }  
 
-  toggleState() {
+    toggleState() {
       this.setState({ isOpened: !this.state.isOpened });
-  }
+    }
 
-  render() {
-    console.log( 'isOpened', this.state.isOpened );
-    return <div onClick={this.toggleState.bind(this)}>Its dropdown</div>;
+    render() {
+        console.log( 'isOpened', this.state.isOpened );
+        let dropdownText;
+        if (this.state.isOpened) {
+            dropdownText = <div>Here is what is show in dropdown</div>
+        }
+    return (
+        <div onClick={this.toggleState.bind(this)}>
+            Its dropdown
+            {dropdownText}
+        </div>
+    );
   }
 }
 
