944cbb608a79ade33990e928a28e24c8be15a07b
Dmitry <gorbunov.webdev@gmail.com>
2018-02-12 00:39:41 +0400

    added variable to state

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Dropdown extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { isOpened: false };
+  }  
+
   render() {
     return <div>Its dropdown</div>;
   }
