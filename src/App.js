4bd3ea7afbc8aa3286229b920d6d4a5a8fca169c
Dmitry <gorbunov.webdev@gmail.com>
2018-02-13 23:05:50 +0400

    Added array for links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
+const menu = [
+  {
+    link: '/articles',
+    label: 'Articles'
+  },
+  {
+    link: '/contacts',
+    label: 'Contacts'
+  },
+  {
+    link: '/posts',
+    label: 'Posts'
+  }
+];
 
 class App extends Component {
   render () {
